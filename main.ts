e1c98480c8da8d6b537ac5749087ce5ff7b3be34
Yanu Widodo <10122431+ynwd@users.noreply.github.com>
2023-07-31 17:35:00 +0700

    refactor: update fastro version

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import Server, {
   Context,
   HttpRequest,
   RenderOptions,
-} from "https://deno.land/x/fastro@v0.77.5/mod.ts";
+} from "https://deno.land/x/fastro@v0.77.6/mod.ts";
 import app from "./pages/app.tsx";
 
 // Create a server instance
@@ -11,11 +11,10 @@ const s = new Server();
 // Simulate async method
 const getUser = (data?: string) => Promise.resolve(data);
 
-// The endpoint for the root URL
-s.get(
-  "/",
-  () => "To try the URL pattern, please open: http://localhost:8000/users/123",
-);
+// Redirect to http://localhost:8000/users/123
+s.get("/", () => {
+  return Response.redirect("http://localhost:8000/users/123", 307);
+});
 
 // Create an SSR page endpoint
 s.page(
@@ -28,17 +27,17 @@ s.page(
   // The page handler
   async (req: HttpRequest, ctx: Context) => {
     // Get data from async method with url params
-    const data = await getUser(req.params?.id);
+    const user = await getUser(req.params?.id);
 
     const options: RenderOptions = {
       // Inject data to component via props options
-      props: { data },
+      props: { data: user },
       // set HTTP status
       // You can change with another
       status: 200,
       // Put your SEO setting here
       // You can change the value dynamically
-      html: { head: { title: "React Component" } },
+      html: { head: { title: `Hello, User #${user}` } },
     };
 
     // Render react component from server
